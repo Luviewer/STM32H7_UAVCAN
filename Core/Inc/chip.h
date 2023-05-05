88f32d76466ba9bfdd23ceacaa74680229f5ad73
Luviewer <liaoluweillw@163.com>
2023-05-05 18:42:20 +0800

    init

---
+++ Core/Inc/chip.h
@@ -0,0 +1,17 @@
+/*
+ * chip.h
+ *
+ *  Created on: 2023年4月3日
+ *      Author: Luviewer
+ */
+
+#ifndef INC_CHIP_H_
+#define INC_CHIP_H_
+
+#include "stm32h743xx.h"  // HAL header file
+#include "stm32h7xx_hal.h"
+
+#define STM32_PCLK1           (120000000ul)          // 42 MHz
+#define STM32_TIMCLK1         (240000000ul)          // 84 MHz
+
+#endif /* INC_CHIP_H_ */
